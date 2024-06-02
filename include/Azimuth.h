4d97472e1c48a544d809d2ce886ea6eccc6c6f19
tomasvdlaan <tomasvlaan@gmail.com>
2024-06-02 22:11:54 +0200

    added Azimuth header file

---
+++ include/Azimuth.h
@@ -0,0 +1,17 @@
+/*******************************************************************
+ *  Definitions and constants
+ *
+ *******************************************************************/
+#ifndef AZIMUTH_HEADER
+#define AZIMUTH_HEADER
+
+/*******************************************************************
+ *  Storage keys and defaults
+ *******************************************************************/
+#define JSON_AZIMUTH_LEFT "azimuth_left"
+#define JSON_AZIMUTH_LEFT_DEFAULT 0
+
+#define JSON_AZIMUTH_RIGHT "azimuth_right"
+#define JSON_AZIMUTH_RIGHT_DEFAULT 5
+
+#endif
--- src/Azimuth.cpp
+++ src/Azimuth.cpp
@@ -1,3 +1,5 @@
+#include "Azimuth.h"
+
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include "Storage.h"
@@ -7,8 +9,6 @@
  * Setup variables
  ********************************************************************/
 static JsonDocument azimuth_data;
-#define JSON_AZIMUTH_LEFT "azimuth-left"
-#define JSON_AZIMUTH_RIGHT "azimuth-right"
 
 static void AZIMUTH_init_int(const char *key, int default_value)
 {
@@ -23,17 +23,17 @@ static void AZIMUTH_init_int(const char *key, int default_value)
 
 static void AZIMUTH_setup_variables(void)
 {
-    AZIMUTH_init_int(JSON_AZIMUTH_LEFT, 0);
-    AZIMUTH_init_int(JSON_AZIMUTH_RIGHT, 5);
+    AZIMUTH_init_int(JSON_AZIMUTH_LEFT, JSON_AZIMUTH_LEFT_DEFAULT);
+    AZIMUTH_init_int(JSON_AZIMUTH_RIGHT, JSON_AZIMUTH_RIGHT_DEFAULT);
 }
 
 /********************************************************************
  * Azimuth main loop
  ********************************************************************/
-bool AZIMUTH_ENABLED = false;
+static bool AZIMUTH_ENABLED = false;
 void AZIMUTH_update()
 {
-    if(AZIMUTH_ENABLED)
+    if (AZIMUTH_ENABLED)
     {
         float wheel_position = WHEEL_get_position();
         float azimuth_position = wheel_position * (float(azimuth_data[JSON_AZIMUTH_RIGHT]) - float(azimuth_data[JSON_AZIMUTH_LEFT])) + azimuth_data[JSON_AZIMUTH_LEFT];
@@ -41,16 +41,17 @@ void AZIMUTH_update()
     }
 }
 
-
 /********************************************************************
  * Azimuth enable/disable
  ********************************************************************/
-void AZIMUTH_enable() {
+void AZIMUTH_enable()
+{
     AZIMUTH_ENABLED = true;
     ANALOG_OUT_enable();
 }
 
-void AZIMUTH_disable() {
+void AZIMUTH_disable()
+{
     AZIMUTH_ENABLED = false;
     ANALOG_OUT_enable();
 }
